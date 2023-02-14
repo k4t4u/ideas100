e077d5ca53d46e2454966154ddad9e4fd023aa60
k4t4u <115025099+k4t4u@users.noreply.github.com>
2023-02-14 19:48:47 +0100

    1.0.2. (translates and fixed questionPage)

--- src/main/java/pl/k4t/ideas100/service/QuestionService.java
+++ src/main/java/pl/k4t/ideas100/service/QuestionService.java
@@ -16,23 +16,25 @@ public class QuestionService {
     private QuestionRepository questionRepository;
 
     public QuestionService(QuestionRepository questionRepository) {
+
         this.questionRepository = questionRepository;
     }
 
     @Transactional(readOnly = true)
     public List<Question> getQuestions() {
+
         return questionRepository.findAll();
     }
 
     @Transactional(readOnly = true)
     public Question getQuestion(UUID id) {
+
         return questionRepository.getById(id);
     }
 
     @Transactional
     public Question createQuestion(Question questionRequest) {
         Question question = new Question();
-
         question.setName(questionRequest.getName());
 
         return questionRepository.save(question);
@@ -41,7 +43,6 @@ public class QuestionService {
     @Transactional
     public Question updateQuestion(UUID id, Question questionRequest) {
         Question question = questionRepository.getById(id);
-
         question.setName(questionRequest.getName());
 
         return questionRepository.save(question);
@@ -54,16 +55,19 @@ public class QuestionService {
 
     @Transactional(readOnly = true)
     public List<Question> findAllByCategoryId(UUID id) {
+
         return questionRepository.findAllByCategoryId(id);
     }
 
     @Transactional(readOnly = true)
     public Page<Question> findHot(Pageable pageable) {
+
         return questionRepository.findHot(pageable);
     }
 
     @Transactional(readOnly = true)
     public Page<Question> findUnanswered(Pageable pageable) {
+
         return questionRepository.findUnanswered(pageable);
     }
 
