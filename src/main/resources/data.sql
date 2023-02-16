10a9a975295421b4744c378fa290751d7af4af08
k4t4u <115025099+k4t4u@users.noreply.github.com>
2023-02-16 18:14:44 +0100

    1.0.3. (Index and SingleView changes)

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -33,3 +33,18 @@ insert into questions (id, name, category_id) values
 insert into questions (id, name, category_id) values
                                                   (gen_random_uuid(), 'Dlaczego warto uczyć się programowania', (select id from categories where name = 'Edukacja')),
                                                   (gen_random_uuid(), 'Dlaczego Java jest dobrym językiem na start', (select id from categories where name = 'Edukacja'));
+
+insert into questions (id, name, category_id) values
+    (gen_random_uuid(), 'Jakie są najzdrowsze warzywa?', (select id from categories where name = 'Zdrowie'));
+
+insert into answers (id, name, question_id) values
+                                                (gen_random_uuid(), 'Marchewka', (select id from questions where name = 'Jakie są najzdrowsze warzywa?')),
+                                                (gen_random_uuid(), 'Brokuł', (select id from questions where name = 'Jakie są najzdrowsze warzywa?')),
+                                                (gen_random_uuid(), 'Dynia', (select id from questions where name = 'Jakie są najzdrowsze warzywa?')),
+                                                (gen_random_uuid(), 'Groch', (select id from questions where name = 'Jakie są najzdrowsze warzywa?'));
+
+
+insert into answers (id, name, question_id) values
+                                                (gen_random_uuid(), 'Gdańsk', (select id from questions where name = 'Gdzie najlepiej spędzić wakacje z Polsce')),
+                                                (gen_random_uuid(), 'Bieszczady', (select id from questions where name = 'Gdzie najlepiej spędzić wakacje z Polsce')),
+                                                (gen_random_uuid(), 'Mazury', (select id from questions where name = 'Gdzie najlepiej spędzić wakacje z Polsce'));
